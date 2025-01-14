0f8c391f7684c9efafcebe33097a45fe95851bd5
javanhut <javan.hutchinson18@gmail.com>
2025-01-13 22:49:12 -0500

    fix: fixed the print statement

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -157,6 +157,15 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 				return applyFunction(spell, args)
 			}
 		}
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
 	}
 	return NONE
 }
