84a2c4ce7bddc69a196b510daafe2a966771f114
javanhut <javan.hutchinson18@gmail.com>
2025-02-01 14:58:31 -0500

    Added a fix for the raise and a new test case

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -686,14 +686,12 @@ func evalProgram(program *ast.Program, env *object.Environment) object.Object {
 	var result object.Object
 
 	for _, statement := range program.Statements {
-
 		result = Eval(statement, env)
 
-		switch result := result.(type) {
+		switch result.(type) {
 		case *object.ReturnValue:
-			return result.Value
-		case *object.Error:
-
+			return result.(*object.ReturnValue).Value
+		case *object.Error, *object.CustomError:
 			return result
 		}
 	}
