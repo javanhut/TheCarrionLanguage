7e1698d90439eef92f018e3f74480383bf0f57d1
javanhut <javan.hutchinson18@gmail.com>
2025-02-23 14:25:48 -0500

    fix: fixed string comparison

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -1040,13 +1040,21 @@ func evalStringInfixExpression(
 	operator string,
 	left, right object.Object,
 ) object.Object {
-	if operator != "+" {
+	leftStr := left.(*object.String).Value
+	rightStr := right.(*object.String).Value
+	switch operator {
+	case "+":
+		return &object.String{Value: leftStr + rightStr}
+
+	case "==":
+		return nativeBoolToBooleanObject(leftStr == rightStr)
+	case "!=":
+		return nativeBoolToBooleanObject(leftStr != rightStr)
+
+	default:
 		return newError("unknown operator: %s %s %s",
 			left.Type(), operator, right.Type())
 	}
-	leftVal := left.(*object.String).Value
-	rightVal := right.(*object.String).Value
-	return &object.String{Value: leftVal + rightVal}
 }
 
 func evalBooleanInfixExpression(operator string, left, right object.Object) object.Object {
